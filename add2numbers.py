2a1278c856823643a5fb07ae1b72959e3728fc86
tolinachali <tolexchali40@gmail.com>
2024-03-27 11:21:08 +0300

    This is my frist commit

---
+++ add2numbers.py
@@ -0,0 +1,9 @@
+# Enter two numbers 
+n1 = input('Enter number 1: ')
+n2 = input('Enter number 2: ')
+
+# Add them together
+sum = float(n1) + float(n2)
+
+#Display the result
+print('The sum of {0} and {1} is {2}'.format(n1, n2, sum))
